eb9e03ee96aba4348834022e96ef0c62b5984908
VPaulineC <vp2765@mci4me.at>
2024-04-12 15:44:52 +0200

    Ordner figures erstellt

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sort import bubble_sort
 from load_data import load_data
+import os 
+
 #Daten laden
 data = load_data("activity.csv")
 PowerOriginal = data["PowerOriginal"]
@@ -12,3 +14,9 @@ sorted_PowerOriginal = bubble_sort(PowerOriginal)
 # Leistungskurve anzeigen
 plt.plot(PowerOriginal[::-1])
 plt.show()
+
+
+if not os.path.exists("figures"):
+    os.makedirs("figures")
+
+plt.savefig("figures/Leistungskurve.png")
