87bf92c4ced082f9de49204848277de7cde60fd2
VPaulineC <vp2765@mci4me.at>
2024-04-12 13:27:01 +0200

    bubble sort

--- sort.py
+++ sort.py
@@ -15,3 +15,16 @@ sorted_rows = bubble_sort(rows)
 # Sortierte Daten ausgeben
 for data in sorted_rows:
     print(data)
+
+#oder:
+#def bubble_sort(arr):
+    #arr_len = len(arr)
+    #for i in range(arr_len-1):
+        #flag = 0
+        #for j in range(0, arr_len-i-1):
+            #if arr[j] > arr[j+1]:
+                #arr[j+1], arr[j] = arr[j], arr[j+1]
+                #flag = 1
+                #if flag == 0:
+                    #break
+    #return arr
