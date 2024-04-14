396774f3431d73b3599177a64d7bac1bf43bd873
carinatilg <tc4640@mci4me.at>
2024-04-14 18:46:22 +0200

    Kommentare in main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sort import bubble_sort
 from load_data import load_data
 import os 
 
-#Daten laden
+# Daten laden
 data = load_data("activity.csv")
 PowerOriginal = data["PowerOriginal"]
 
@@ -14,7 +14,7 @@ sorted_PowerOriginal = bubble_sort(PowerOriginal)
 # Leistungskurve anzeigen
 plt.plot(PowerOriginal[::-1])
 
-
+# Ordner für Grafik erstellen und speichern der Grafik
 if not os.path.exists("figures"):
     os.makedirs("figures")
 
